908a993c76ca9dcf483a6ae5458cb573f5fb285b
Student <example@example.com>
2020-03-11 09:16:13 -0700

    initial commit

---
+++ TravelDiary.Tests/ModelTests/PlaceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using TravelDiary.Models;
+
+namespace TravelDiary.Tests
+{
+  [TestClass] 
+  public class PlaceTests
+  {
+
+  }
+}
