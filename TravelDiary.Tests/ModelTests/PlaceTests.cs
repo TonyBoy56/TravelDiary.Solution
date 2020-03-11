cfc0a8a6c334da0861010e46f4287bfc59537546
Student <example@example.com>
2020-03-11 09:55:10 -0700

    pass all tests

--- TravelDiary.Tests/ModelTests/PlaceTests.cs
+++ TravelDiary.Tests/ModelTests/PlaceTests.cs
@@ -6,8 +6,81 @@ using TravelDiary.Models;
 namespace TravelDiary.Tests
 {
   [TestClass] 
-  public class PlaceTests
+  public class PlaceTests : IDisposable 
   {
 
+    public void Dispose()
+    {
+      Place.ClearAll();
+    }
+
+
+    [TestMethod]
+    public void PlaceConstructor_CreateInstanceOfPlace_Place()
+    {
+      Place newPlace = new Place("test");
+      Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
+
+    [TestMethod]
+    public void PlaceList_ReturnsEmptyList_PlaceList()
+    {
+      //Arrange
+      List<Place> newPlace = new List<Place> {};
+
+      //Act
+      List<Place> result = Place.Instances;
+
+      //Assert
+      CollectionAssert.AreEqual(newPlace, result);
+    }
+    
+
+    [TestMethod]
+    public void PlaceList_ReturnsPlaces_PlaceList()
+    {
+      //Arrange
+      string cityName1 = "Paris";
+      string cityName2 = "Sydney";
+      Place newPlace1 = new Place(cityName1);
+      Place newPlace2 = new Place(cityName2);
+      List<Place> newPlace = new List<Place> { newPlace1, newPlace2 };
+
+      //Act
+      List<Place> result = Place.Instances;
+
+      //Assert
+      CollectionAssert.AreEqual(newPlace, result);
+    }
+
+    [TestMethod]
+    public void GetId_PlacesInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string cityName = "Las Vegas";
+      Place newPlace = new Place(cityName);
+
+      //Act
+      int result = newPlace.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectPlace_Place()
+    {
+      //Arrange
+      string cityName1 = "Paris";
+      string cityName2 = "Sydney";
+      Place newPlace1 = new Place(cityName1);
+      Place newPlace2 = new Place(cityName2);
+
+      //Act
+      Place result = Place.Find(2);
+
+      //Assert
+      Assert.AreEqual(newPlace2, result);
+    }
   }
 }
--- TravelDiary/Models/Place.cs
+++ TravelDiary/Models/Place.cs
@@ -4,6 +4,25 @@ namespace TravelDiary.Models
 {
   public class Place
   {
+    public string CityName { get; set; }
+    public int Id { get; }
+    public static List<Place> Instances { get; } = new List<Place> {};
 
+    public Place(string cityName)
+    {
+      CityName = cityName;
+      Instances.Add(this);
+      Id = Instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      Instances.Clear();
+    }
+
+    public static Place Find(int searchId)
+    {
+      return Instances[searchId-1];
+    }
   }
 }  
